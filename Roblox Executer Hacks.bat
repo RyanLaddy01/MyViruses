b297225b12e9b5c7042a64795e5d0e3ef3e6b081
RyanLaddy01 <ryanladdy@outlook.com>
2024-10-30 21:20:20 +0000

    Fake Roblox Hacks Batch File Malware

---
+++ Roblox Executer Hacks.bat
@@ -0,0 +1,38 @@
+@echo off
+
+
+title Gathering Information...
+color 4
+echo Gathering your information...
+timeout /t 1 /nobreak >nul
+
+echo.
+echo [INFO] Retrieving IP Address...
+timeout /t 1 /nobreak >nul
+echo [INFO] IP Address: 192.168.0.101
+
+echo.
+echo [INFO] Gathering Full Name...
+timeout /t 1 /nobreak >nul
+echo [INFO] Full Name Was Successfully Sent To Anonymous.
+
+echo.
+echo [INFO] Gathering Full Cookies And Site Data...
+timeout /t 1 /nobreak >nul
+echo [INFO] Cookies And Site Data Was Successfully Sent To Anonymous.
+
+echo.
+echo [INFO] Locating Your Location...
+timeout /t 1 /nobreak >nul
+echo [INFO] Location Was Successfully Located.
+
+echo.
+echo [INFO] Compiling Information...
+timeout /t 1 /nobreak >nul
+echo [INFO] Successfully Complied Information.
+
+echo.
+echo Your information has been sent to the sender.
+echo Your Information Has Been Stolen
+timeout /t 30 /nobreak >nul
+exit
